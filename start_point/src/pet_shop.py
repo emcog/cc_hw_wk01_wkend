9a29b758341d7b5d290cfb7744c03e69bcab297b
emCog <robin.leonard@gmail.com>
2020-11-21 22:06:05 +0000

    test 2 – psudo code

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -3,5 +3,9 @@
 def get_pet_shop_name(setUp):
     name = setUp['name']
     return name
-    print(name)
-    
+    print(name) #sanity check
+
+def get_total_cash(setUp):
+    # get total chash
+    #  function dictionary - list - dictionary
+    #  setUp['customers']
